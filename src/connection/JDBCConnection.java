b16fcc2d52ac7268dd8a3b9229aea267f1f17028
Sanjaytholani <sanjaytholani.m@gmail.com>
2022-12-04 20:45:46 -0500

    jdbc connection added

---
+++ src/connection/JDBCConnection.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sanjaytholani
+ */
+public class JDBCConnection {
+    public static Connection Connect(){
+        try{
+       Class.forName("com.mysql.jdbc.Driver");
+       System.out.println("connection to db");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/AED_Final_Project", "root", "root@123");
+            System.out.println("connected ");
+        Statement statement = (Statement) connection.createStatement();
+//        ResultSet resultSet = statement.executeQuery("Select * from hospital.login");
+                                                         
+//        while(resultSet.next()){
+//            System.out.println("Hi");
+//            System.out.print(resultSet.getString("login.role"));
+            return connection;
+//        }
+        }
+        catch(ClassNotFoundException | SQLException e){
+            return null;
+        }
+    }
+}
